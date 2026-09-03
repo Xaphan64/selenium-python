ba1c2b2ba5d0504fff1a9a6277a156542810933f
Xaphan64 <alex_daniel64@yahoo.com>
2026-09-03 09:39:40 +0300

    started python tutorial

---
+++ FirstDemo.py
@@ -0,0 +1,15 @@
+print("test")
+
+# comments
+
+a = 3
+print(a)
+b, c, d = 5, 6.4, "great"
+
+#  this will throw error
+# print("value is " + b)
+
+test = "{} {}".format("Value is", b)
+print(test)
+
+print(f"value is {b}")
